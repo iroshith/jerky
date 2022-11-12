703bcca18560a33c1fc06d648f03968cbcba418c
Hiroshi Takatoh <hiroshi.takatoh@MacBook-Air.local>
2022-11-12 08:26:16 -0800

    initial commit!

---
+++ pages/utility/irobot-start.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from "react";
+
+export default function irobotStart() {
+    return <h1>Start irobot</h1>;
+  }
